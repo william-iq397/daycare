e512d8fccd5cfc2de73da65b5e060d44795e1c59
william-iq397 <93004634+william-iq397@users.noreply.github.com>
2024-12-08 20:35:01 +0300

    .env update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,16 @@
-import { defineNuxtConfig } from "nuxt/config"
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-    compatibilityDate: "2024-04-03",
-    devtools: { enabled: true },
-    modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
-    supabase: {
-        redirect: false,
-        redirectOptions: {
-            login: "/login",
-            callback: "/confirm",
-            include: undefined,
-            exclude: [],
-            cookieRedirect: false,
-        },
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  supabase: {
+    redirect: false,
+    redirectOptions: {
+      login: "/login",      // Redirect here after login if not logged in
+      callback: "/confirm", // Redirect after Supabase auth callback
+      exclude: [],          // Paths that skip redirection logic
+      cookieRedirect: false // Disable cookie-based redirection
     },
-})
+  },
+});
